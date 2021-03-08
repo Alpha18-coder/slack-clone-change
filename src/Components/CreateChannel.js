d331674a5059736474ee7b10ff92204a793e35c3
Alonso <alonsosandovalch@gmail.com>
2021-03-08 12:25:38 -0500

    Slack Clone Challenge Final version

--- src/Components/CreateChannel.js
+++ src/Components/CreateChannel.js
@@ -4,12 +4,12 @@ import styled from "styled-components";
 function CreateChannel() {
     return (
         <Container>
-        <Content>
-            <SlackImg src="https://cdn.worldvectorlogo.com/logos/slack-new-logo.svg" alt="Slack logo"/>
-            <h1>Slack Clone</h1>
-            <h2>Create a Channel</h2>
-        </Content>
-    </Container>
+            <Content>
+                <SlackImg src="https://cdn.worldvectorlogo.com/logos/slack-new-logo.svg" alt="Slack logo" />
+                <h1>Slack Clone</h1>
+                <h2>Create a Channel</h2>
+            </Content>
+        </Container>
     )
 }
 
@@ -20,6 +20,7 @@ width: 100%;
 height: 100vh;
 background-image: url("https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v748-toon-106_1_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=af38437b7941dbdaa625d8cf5802f9eb");
 background-repeat: no-repeat;
+background-size: 200vh;
 display: flex;
 justify-content: center;
 align-items: center;
